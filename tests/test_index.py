06953d60aeb85261fea916144baa90b49eaec41c
Ismael Pamplona <ismaelpamplona@hotmail.com>
2024-09-29 22:02:12 -0300

    (up) Issue #setup_flask_project.

---
+++ tests/test_index.py
@@ -0,0 +1,22 @@
+import sys
+from os.path import abspath, dirname
+
+import pytest
+
+sys.path.insert(0, abspath(dirname(dirname(__file__))))
+
+from app import create_app
+
+
+@pytest.fixture
+def client():
+    app = create_app()
+    app.config["TESTING"] = True
+    with app.test_client() as client:
+        yield client
+
+
+def test_welcome_route(client):
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.data == b"Welcome to the Task Management App!"
